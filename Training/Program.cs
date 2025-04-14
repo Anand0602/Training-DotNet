3019fc36c1f052efed7ed98eb8a0d3769d3d1c54
Anand0602 <sharmaanand46444@gmail.com>
2025-04-14 16:17:16 +0530

    Normal Changes

--- Training/Program.cs
+++ Training/Program.cs
@@ -21,3 +21,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+// This is a simple ASP.NET Core MVC application that serves as a starting point for building web applications.
